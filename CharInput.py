452d958362219635069183a25fa5563392b7c8f3
santhiya1214 <54829544+santhiya1214@users.noreply.github.com>
2020-04-10 22:40:43 +0530

    Character Input Sample program

--- CharInput.py
+++ CharInput.py
@@ -1,8 +1,7 @@
 ## First method of implementation
 ## Used input(), \t, \n, int(), str()
 
-UserName = input('Please Enter your Name ')
+UserName = raw_input('Please Enter your Name ')
 UserAge = 100 - int(input('Please Enter your Age '))
 num = int(input('Please enter how many time you want to print'))
 print(num * ('you will turn 100 years old in'+'\t' +str(UserAge) +'\n'))
-
